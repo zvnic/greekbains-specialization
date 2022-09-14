5284e73ba27145f6c3dd45fe9359186fe07eb478
zvnic <zvnman@yandex.ru>
2022-09-14 19:55:53 +0300

    Enter word to array

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("#Task");
+
+string[] GetNewArray() {
+    int randomArraySize = new Random().Next(1,10);
+    string[] str = new string[randomArraySize];
+    return str;
+}
+
+string[] EnterWordToArray(string[] str) {
+
+    Console.WriteLine($"Введите слова в количестве {str.Length} шт.:");
+
+    for(int i = 0; i < str.Length; i++) {
+        Console.Write($"Введите {i+1} слово: ");
+        str[i] = Console.ReadLine();
+    }
+
+    return str;
+}
+
+
